8bd13008a2db975f24477d6626a7ced34306dbe9
Gokul <gokulr488@gmail.com>
2021-10-27 09:42:01 +0530

    check if already logged in

--- lib/Screens/LandingScreens/LoadingScreen.dart
+++ lib/Screens/LandingScreens/LoadingScreen.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fleezy_web/Components/BaseScreen.dart';
 import 'package:fleezy_web/Components/LoadingDots.dart';
+import 'package:fleezy_web/Screens/HomePage/HomeScreen.dart';
 import 'package:fleezy_web/Screens/LandingScreens/LoginScreen.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +16,7 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   bool _initialized = false;
   bool _error = false;
+  bool _isLoggedIn = false;
 
   void initializeFlutterFire() async {
     try {
@@ -28,11 +31,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     initializeFlutterFire();
+    checkLoggedInStatus();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    if (_isLoggedIn) return HomeScreen();
     if (_error) {
       return const BaseScreen(
           child: Center(child: LoadingDots(size: 75)),
@@ -45,5 +50,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
     }
 
     return LoginScreen();
+  }
+
+  void checkLoggedInStatus() {
+    // FirebaseFirestore.instance.settings =
+    //     Settings(host: '10.0.2.2:8080', sslEnabled: false);
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    if (_auth.currentUser != null) {
+      //Provider.of<UiState>(context, listen: false).setIsAdmin(true);
+      _isLoggedIn = true;
+    } else {
+      _isLoggedIn = false;
+    }
+    setState(() {});
   }
 }
